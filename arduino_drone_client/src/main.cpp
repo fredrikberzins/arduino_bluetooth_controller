b2c67d204a1f29313e342b16628ee1987705788c
02TheBear <Fredrik.Berzins@gmail.com>
2020-10-15 12:21:03 +0200

    removing host code

--- arduino_drone_client/src/main.cpp
+++ arduino_drone_client/src/main.cpp
@@ -1,10 +1,9 @@
 #include <Arduino.h>
 
-#include <string> // allow for use of string when sending info over bluetooth
 #include <Wire.h> // Libery for gyro and acelerometer
-#include <SoftwareSerial.h> // libery for Bluetooth module(HC-06)
+//# #include <SoftwareSerial.h> // libery for Bluetooth module(HC-06)
 
-SoftwareSerial BT(0, 1); 
+//# SoftwareSerial BT(0, 1); 
 // creates a "virtual" serial port/UART
 // connect BT module RX to Digital TX0
 // connect BT module TX to Digital RX1
@@ -19,12 +18,23 @@ SoftwareSerial BT(0, 1);
 #define Motor6 9 // Motor 4 Digital pin 9
 #define MotorArm 2 // Motor Arming Digital pin 2
 
-long accelX, accelY, accelZ;
-float gForceX, gForceY, gForceZ;
+//# long accelX, accelY, accelZ;
+//# float gForceX, gForceY, gForceZ;
 
-long gyroX, gyroY, gyroZ;
-float rotX, rotY, rotZ;
+//# long gyroX, gyroY, gyroZ;
+//# float rotX, rotY, rotZ;
 
+//# char Rx_input[4]; //Initializ variable for Serial transfer (over wire)
+
+//# char mode[5] = "TAER";
+
+//# int throttle = 0;
+//# int roll = 0;
+//# int pitch = 0;
+//# int yaw = 0;
+
+int brightness = 0;
+int fadeAmount = 5;
 
 void setup()  
 {
@@ -37,69 +47,23 @@ void setup()
   pinMode(Motor6, OUTPUT);
   pinMode(MotorArm, OUTPUT);
 
-  // set the data rate for the serial port/UART, for Bluetooth module.
-  BT.begin(9600);
-
   // set the data rate for the serial port/UART, for accelerometer and gyro module.
   Serial.begin(9600);
-  Wire.begin();
-  setupMPU_6050();
-
-  // Send test message to other device
-  BT.println("Reciver: Setup done");
+  //# Wire.begin();
+  //# setup_MPU_6050();
+  digitalWrite(MotorArm, HIGH);
 }
-
-void setupMPU_6050(){
-  Wire.beginTransmission(0b1101000); //This is the I2C address of the MPU (b1101000/b1101001 for AC0 low/high datasheet sec. 9.2)
-  Wire.write(0x6B); //Accessing the register 6B - Power Management (Sec. 4.28)
-  Wire.write(0b00000000); //Setting SLEEP register to 0. (Required; see Note on p. 9)
-  Wire.endTransmission();  
-  Wire.beginTransmission(0b1101000); //I2C address of the MPU
-  Wire.write(0x1B); //Accessing the register 1B - Gyroscope Configuration (Sec. 4.4) 
-  Wire.write(0x00000000); //Setting the gyro to full scale +/- 250deg./s 
-  Wire.endTransmission(); 
-  Wire.beginTransmission(0b1101000); //I2C address of the MPU
-  Wire.write(0x1C); //Accessing the register 1C - Acccelerometer Configuration (Sec. 4.5) 
-  Wire.write(0b00000000); //Setting the accel to +/- 2g
-  Wire.endTransmission(); 
-}
-
-
-string Bluetooth_feed; // stores incoming character from other device
 
 void loop() 
 {
-  if (BT.available())
-  // if text arrived in from BT serial...
-  {
-    a=(BT.read());
-    if (a=='1')
-    {
-      digitalWrite(SW, LOW);// Turn LIGH ON
-      BT.println("Light on");
-    }
-    if (a=='2')
-    {
-      digitalWrite(SW, HIGH);// Turn LIGHT OFF
-      BT.println("Light off");
-    }
-    if (a=='?')
-    {
-      BT.println("Send '1' to turn LIGHT on");
-      BT.println("Send '2' to turn LIGHT OFF");
-    }   
-    if (a=='3')
-    {
-      digitalWrite(SW, HIGH);// Turn LIGHT OFF
-      BT.println("Light off");
-      delay(500);
-      digitalWrite(SW, LOW);// Turn LIGHT OFF
-      delay(500);
-      digitalWrite(SW, HIGH);// Turn LIGHT OFF
-      delay(500);
-      digitalWrite(SW, LOW);// Turn LIGHT OFF
-    }
-    
+  //# Serial.readBytes(Rx_input, 4); //Read the serial data and store in var
+  //# Serial.println(Rx_input); //Print data on Serial Monitor
+  analogWrite(Motor1, brightness);
+  // change the brightness for next time through the loop:
+  brightness = brightness + fadeAmount;
+  // reverse the direction of the fading at the ends of the fade:
+  if (brightness == 0 || brightness == 255) {
+    fadeAmount = -fadeAmount;
   }
+  delay(30);
 }
-  
