88cae9a2440cff00d4076b343c31d8e7f20fe330
02TheBear <Fredrik.Berzins@gmail.com>
2020-11-05 12:24:44 +0100

    change from drone to bluetooth controller and initial set up for pin in and out

--- README.md
+++ README.md
@@ -1,4 +1,12 @@
-# arduino_drone
-Program a arduino into a Flight controller with the help of a PWM reciver binded to a transmitter.
+# Arduiono bluetooth controller
 
-The FC is not ment for autolevel fligt.
+A bluetooth controler made with 2 joyticks and 2 buttons. 
+
+Parts
+- 2x HS-05(Bluetooth module)
+- Arduino pro micro(Almost every ardouno works with 4 digitla pins and 4 analog. May need change to platformio.ini file.)
+- Power source/Battery (5V or higher)
+- Tiny Buck converter(If power source voltage is higher then 5V)
+
+Output signal
+The output is sent as a digital signal 
--- arduino_drone_client/src/main.cpp
+++ arduino_drone_client/src/main.cpp
@@ -1,69 +1,38 @@
 #include <Arduino.h>
 
-#include <Wire.h> // Libery for gyro and acelerometer
-//# #include <SoftwareSerial.h> // libery for Bluetooth module(HC-06)
+#define right_x_axis A0
+#define right_y_axis A1
+#define left_x_axis A2
+#define left_y_axis A3
 
-//# SoftwareSerial BT(0, 1); 
-// creates a "virtual" serial port/UART
-// connect BT module RX to Digital TX0
-// connect BT module TX to Digital RX1
-// connect BT GND to GND
-// connect BT Vcc to 5V
+#define right_switch 2
+#define left_switch 3
 
-#define Motor1 4 // Motor 1 Digital pin 4
-#define Motor2 5 // Motor 2 Digital pin 5
-#define Motor3 6 // Motor 3 Digital pin 6
-#define Motor4 7 // Motor 4 Digital pin 7
-#define Motor5 8 // Motor 3 Digital pin 8
-#define Motor6 9 // Motor 4 Digital pin 9
-#define MotorArm 2 // Motor Arming Digital pin 2
-
-//# long accelX, accelY, accelZ;
-//# float gForceX, gForceY, gForceZ;
-
-//# long gyroX, gyroY, gyroZ;
-//# float rotX, rotY, rotZ;
-
-//# char Rx_input[4]; //Initializ variable for Serial transfer (over wire)
-
-//# char mode[5] = "TAER";
-
-//# int throttle = 0;
-//# int roll = 0;
-//# int pitch = 0;
-//# int yaw = 0;
-
-int brightness = 0;
-int fadeAmount = 5;
+#define BT_out 0
+#define BT_in 1
 
 void setup()  
 {
-  // set digital pin to control as an output for motors
-  pinMode(Motor1, OUTPUT);
-  pinMode(Motor2, OUTPUT);
-  pinMode(Motor3, OUTPUT);
-  pinMode(Motor4, OUTPUT);
-  pinMode(Motor5, OUTPUT);
-  pinMode(Motor6, OUTPUT);
-  pinMode(MotorArm, OUTPUT);
+  //set digital pins as input and output for HS-06 (bluetooth module).
+  pinMode(BT_out, OUTPUT);
+  pinMode(BT_in, INPUT);
 
-  // set the data rate for the serial port/UART, for accelerometer and gyro module.
+  //set analog pins as input from joystick.
+  pinMode(right_x_axis, INPUT);
+  pinMode(right_y_axis, INPUT);
+  pinMode(left_x_axis, INPUT);
+  pinMode(left_y_axis, INPUT);
+
+  //set digital pins as input from joystick switchs.
+  pinMode(right_switch, INPUT);
+  pinMode(left_switch, INPUT);
+
+  // set the data rate for the serial monitor.
   Serial.begin(9600);
-  //# Wire.begin();
-  //# setup_MPU_6050();
-  digitalWrite(MotorArm, HIGH);
 }
 
 void loop() 
 {
-  //# Serial.readBytes(Rx_input, 4); //Read the serial data and store in var
-  //# Serial.println(Rx_input); //Print data on Serial Monitor
-  analogWrite(Motor1, brightness);
-  // change the brightness for next time through the loop:
-  brightness = brightness + fadeAmount;
-  // reverse the direction of the fading at the ends of the fade:
-  if (brightness == 0 || brightness == 255) {
-    fadeAmount = -fadeAmount;
-  }
-  delay(30);
+  Serial.println(analogRead(right_x_axis));
+  delay(1000);
 }
