793d24fbc5c21aa50c8fc8c14c53d08c2f6892fc
02TheBear <Fredrik.Berzins@gmail.com>
2020-11-12 22:33:05 +0100

    update to tuning

--- arduino_bluetooth_controller_testing/src/main.cpp
+++ arduino_bluetooth_controller_testing/src/main.cpp
@@ -8,28 +8,46 @@
 #define right_switch_pin 2
 #define left_switch_pin 3
 
+//create max an min varibles
+int min_right_x_axis = 0;
+int max_right_x_axis = 0;
+
+int min_right_y_axis = 0;
+int max_right_y_axis = 0;
+
+int min_left_x_axis = 0;
+int max_left_x_axis = 0;
+
+int min_left_y_axis = 0;
+int max_left_y_axis = 0;
+
+int right_x_axis = 0;
+int right_y_axis = 0;
+int left_x_axis = 0;
+int left_y_axis = 0;
+
 void print_values()
 {
   //a first print to se if Serial.print works (if it all works all values will be 0).
-  Serial.println("right joystick x-axis min: ");
+  Serial.print("right joystick x-axis min: ");
   Serial.print(min_right_x_axis);
   Serial.print(" max: ");
-  Serial.print(max_right_x_axis);
+  Serial.println(max_right_x_axis);
 
-  Serial.println("right joystick y-axis min: ");
+  Serial.print("right joystick y-axis min: ");
   Serial.print(min_right_y_axis);
   Serial.print(" max: ");
-  Serial.print(max_right_y_axis);
+  Serial.println(max_right_y_axis);
 
-  Serial.println("left joystick x-axis min: ");
+  Serial.print("left joystick x-axis min: ");
   Serial.print(min_left_x_axis);
   Serial.print(" max: ");
-  Serial.print(max_left_x_axis);
+  Serial.println(max_left_x_axis);
 
-  Serial.println("left joystick y-axis min: ");
+  Serial.print("left joystick y-axis min: ");
   Serial.print(min_left_y_axis);
   Serial.print(" max: ");
-  Serial.print(max_left_y_axis);
+  Serial.println(max_left_y_axis);
 }
 
 void setup()  
@@ -54,18 +72,7 @@ void setup()
   int right_switch = digitalRead(2);
   int left_switch = digitalRead(3);
 
-  //create max an min varibles
-  int  = 0;
-  int max_right_x_axis = 0;
 
-  int min_right_y_axis = 0;
-  int max_right_y_axis = 0;
-
-  int min_left_x_axis = 0;
-  int max_left_x_axis = 0;
-
-  int min_left_y_axis = 0;
-  int max_left_y_axis = 0;
 
   //set the data rate for the serial monitor.
   Serial.begin(9600);
@@ -79,41 +86,41 @@ void loop()
 {
   //right joystick x-axis
   if (min_right_x_axis > right_x_axis) {
-    min_right_x_axis = right_x_axis
+    min_right_x_axis = right_x_axis;
   }
   
   if (max_right_x_axis > right_x_axis) {
-    max_right_x_axis = right_x_axis
+    max_right_x_axis = right_x_axis;
   }
 
   //right joystick y-axis
 
   if (min_right_y_axis > right_y_axis) {
-    min_right_y_axis = right_y_axis
+    min_right_y_axis = right_y_axis;
   }
   
   if (max_right_y_axis > right_y_axis) {
-    max_right_y_axis = right_y_axis
+    max_right_y_axis = right_y_axis;
   }
 
   //left joystick x-axis
 
     if (min_left_x_axis > left_x_axis) {
-    min_left_x_axis = left_x_axis
+    min_left_x_axis = left_x_axis;
   }
   
   if (max_left_x_axis > left_x_axis) {
-    max_left_x_axis = left_x_axis
+    max_left_x_axis = left_x_axis;
   }
 
   //left joystick y-axis
 
   if (min_left_y_axis > left_y_axis) {
-    min_left_y_axis = left_y_axis
+    min_left_y_axis = left_y_axis;
   }
   
   if (max_left_y_axis > left_y_axis) {
-    max_left_y_axis = left_y_axis
+    max_left_y_axis = left_y_axis;
   }
 
   print_values();
