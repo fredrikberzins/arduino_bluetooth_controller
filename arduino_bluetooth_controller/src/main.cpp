e5c76dea2cf5df0c5966ee983e5063832c7d7283
02TheBear <Fredrik.Berzins@gmail.com>
2020-11-28 00:51:12 +0100

    inital reworkl on main program

--- arduino_bluetooth_controller/src/main.cpp
+++ arduino_bluetooth_controller/src/main.cpp
@@ -1,4 +1,5 @@
 #include <Arduino.h>
+#include <SoftwareSerial.h>
 
 //================
 //    Settings
@@ -8,6 +9,20 @@ Joystick_chanle_maping = "TAER";
 Joystick_stick_mode = 1;
 Joystick_output_mode = "RC";
 
+//Joy stick offset
+//The format is as folowing
+// max/min_right/left_x/y_axis = "int"
+// "int" = offset from what the number is supose to be. can be tested with te testing program.
+//Example: testing program prints 1019 instead of 1023  on the right y axis. 
+int max_right_x_axis = 0;
+int min_right_x_axis = 0;
+int max_right_y_axis = 0;
+int min_right_y_axis = 0;
+int max_left_x_axis = 0;
+int min_left_x_axis = 0;
+int max_left_y_axis = 0;
+int min_left_y_axis = 0;
+
 #define right_x_axis_pin A0
 #define right_y_axis_pin A1
 #define left_x_axis_pin A2
@@ -16,15 +31,34 @@ Joystick_output_mode = "RC";
 #define right_switch_pin 2
 #define left_switch_pin 3
 
-#define BT_out 0
-#define BT_in 1
+//Rx and Tx for bluetooth
+//Recive on pin 0
+//Transmitt on pin 1
+SoftwareSerial Bluetooth (0, 1);
+
+//Defining all input pins for joysticks and buttons
+#define 
+
+#define right_switch_pin 2
+#define left_switch_pin 3
+
+#define right_x_axis analogRead(right_x_axis_pin)
+#define right_y_axis analogRead(right_y_axis_pin)
+#define left_x_axis analogRead(left_x_axis_pin)
+#define left_y_axis analogRead(left_y_axis_pin)
+
+#define right_switch digitalRead(right_switch_pin)
+#define left_switch digitalRead(left_switch_pin)
+
+void send_values()
+{
+  
+}
+
+
 
 void setup()  
 {
-  //set digital pins as input and output for HS-06 (bluetooth module).
-  pinMode(BT_out, OUTPUT);
-  pinMode(BT_in, INPUT);
-
   //set analog pins as input from joystick.
   pinMode(right_x_axis_pin, INPUT);
   pinMode(right_y_axis_pin, INPUT);
@@ -35,27 +69,21 @@ void setup()
   pinMode(right_switch_pin, INPUT);
   pinMode(left_switch_pin, INPUT);
 
-  //set joysticks values as varibles.
-  int right_x_axis = analogRead(A0);
-  int right_y_axis = analogRead(A1);
-  int left_x_axis = analogRead(A2);
-  int left_y_axis = analogRead(A3);
-
-  //set button values as varibles.
-  int right_switch = digitalRead(2);
-  int left_switch = digitalRead(3);
-
-  // set the data rate for the serial monitor.
+  //set the data rate for the serial monitor.
   Serial.begin(9600);
+  //set the data rate for the bluetooth module conection.
+  Bluetooth.begin(9600);
+
+  Serial.println("Ready to use")
+  delay(500);
 }
 
-void Format_signal(Joystick_chanle_maping, Joystick_stick_mode, Joystick_output_mode, right_x_axis, right_y_axis, left_x_axis, left_y_axis)
-{
 
-}
 
 void loop() 
 {
-  Format_signal()
-  delay(100);
+
+  send_values();
+
+  delay(500);
 }
