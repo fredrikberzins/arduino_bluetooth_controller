8f451a64cf59dae05a4497f4ca59c20bef2142ba
02TheBear <Fredrik.Berzins@gmail.com>
2020-11-28 21:45:22 +0100

    +button settings +bluetooth structure +extra comments

--- arduino_bluetooth_controller/src/main.cpp
+++ arduino_bluetooth_controller/src/main.cpp
@@ -4,30 +4,38 @@
 //================
 //    Settings
 //================
+int Joystick_output_mode = 0;
 
-Joystick_chanle_maping = "TAER";
-Joystick_stick_mode = 1;
-Joystick_output_mode = "RC";
+int Button_inversion = 0;
+
+int Button_active_right = 0;
+int Button_active_left = 0;
 
 //Joy stick offset
 //The format is as folowing
 // max/min_right/left_x/y_axis = "int"
 // "int" = offset from what the number is supose to be. can be tested with te testing program.
-//Example: testing program prints 1019 instead of 1023  on the right y axis. 
+//Example: testing program prints 1019 instead of 1023  on the right y-axis, so you put 1023-1019=4 so put max_right_y_axis = 4
+//right x-axis
 int max_right_x_axis = 0;
 int min_right_x_axis = 0;
+//right y-axis
 int max_right_y_axis = 0;
 int min_right_y_axis = 0;
+//left x-axis
 int max_left_x_axis = 0;
 int min_left_x_axis = 0;
+//left y-axis
 int max_left_y_axis = 0;
 int min_left_y_axis = 0;
 
+//Defining input pins for joysticks
 #define right_x_axis_pin A0
 #define right_y_axis_pin A1
 #define left_x_axis_pin A2
 #define left_y_axis_pin A3
 
+//Defining input pins for buttons
 #define right_switch_pin 2
 #define left_switch_pin 3
 
@@ -37,16 +45,13 @@ int min_left_y_axis = 0;
 SoftwareSerial Bluetooth (0, 1);
 
 //Defining all input pins for joysticks and buttons
-#define 
-
-#define right_switch_pin 2
-#define left_switch_pin 3
-
+//Defining joysticks inputs
 #define right_x_axis analogRead(right_x_axis_pin)
 #define right_y_axis analogRead(right_y_axis_pin)
 #define left_x_axis analogRead(left_x_axis_pin)
 #define left_y_axis analogRead(left_y_axis_pin)
 
+//Defining button inputs
 #define right_switch digitalRead(right_switch_pin)
 #define left_switch digitalRead(left_switch_pin)
 
